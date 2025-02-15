14183482628ed4a92b8aa421dad5053b1d97cdbb
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-15 20:35:06 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,35 @@
-import './style.css'
+// import './style.css'
+
+// // const client = new Client()
+// // client.setProject('67b06d59002faaf46bb7')
+
+// import { Client, Databases, ID } from "appwrite";
 
 // const client = new Client()
-// client.setProject('67b06d59002faaf46bb7')
+//     .setEndpoint('https://cloud.appwrite.io/v1')
+//     .setProject('67af7720000426a8a993');
 
+// const databases = new Databases(client);
+
+// const promise = databases.createDocument(
+//   '67b06e5e002b50bbb9d1',
+//   '67b06e7d0021cc4798be',
+//   ID.unique(),
+//   {
+//     "company-name": "SAAN-HUB Solution",
+//     "date-added": new Date(),
+//     "role": "Software Engineer",
+//     "location": "England",
+//     "position-type": "Full-time",
+//     "source": "https://100devs.org"
+//      }
+// );
+
+// promise.then(function (response) {
+//     console.log(response);
+// }, function (error) {
+//     console.log(error);
+// });
 import { Client, Databases, ID } from "appwrite";
 
 const client = new Client()
@@ -12,21 +39,21 @@ const client = new Client()
 const databases = new Databases(client);
 
 const promise = databases.createDocument(
-  '67b06e5e002b50bbb9d1',
-  '67b06e7d0021cc4798be',
+  '67b06e5e002b50bbb9d1',  // Database ID
+  '67b06e7d0021cc4798be',  // Collection ID
   ID.unique(),
   {
     "company-name": "SAAN-HUB Solution",
-    "date-added": new Date(),
+    "date-added": new Date().toISOString(),  // Convert Date to ISO format
     "role": "Software Engineer",
     "location": "England",
     "position-type": "Full-time",
-    "source": "https://100devs.org"
-     }
+    "source": "https://100devs.org"  // Fixed URL
+  }
 );
 
 promise.then(function (response) {
-    console.log(response);
-}, function (error) {
-    console.log(error);
+    console.log("Document created successfully:", response);
+}).catch(function (error) {
+    console.error("Error creating document:", error);
 });
