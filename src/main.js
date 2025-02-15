8c30dea4913521f32772ad65554ed619c72c4cf1
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-15 11:36:14 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ import { Client, Databases, ID } from "appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('67b06d59002faaf46bb7');
+    .setProject('67af7720000426a8a993');
 
 const databases = new Databases(client);
 
 const promise = databases.createDocument(
-    '<DATABASE_ID>',
-    '<COLLECTION_ID>',
+    '67b06e5e002b50bbb9d1',
+    '67b06e7d0021cc4798be',
     ID.unique(),
     { "title": "Hamlet" }
 );
