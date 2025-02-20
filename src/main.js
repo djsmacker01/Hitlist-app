af2802310c2da362d8315a9fb35d929506e8f254
Nurudeen Adedeji <djsmacker01@gmail.com>
2025-02-20 08:01:30 +0000

    commit

--- src/main.js
+++ src/main.js
@@ -93,26 +93,6 @@
 // }
 // addJobsToDom()
 
-// // const promise = databases.createDocument(
-// //     import.meta.env.VITE_APPWRITE_DATABASE_ID,
-// //     import.meta.env.VITE_APPWRITE_COLLECTION_ID,
-// //     ID.unique(),
-// //     {
-// //       "company-name": "SAAN-HUB Solutions",
-// //       "date-added": new Date(),
-// //       "role": "Software Engineer",
-// //       "location": "England",
-// //       "position-type": "Full time",
-// //       "source": "https://100devs.org"
-// //     }
-// // );
-
-// // promise.then(function (response) {
-// //     console.log(response);
-// // }, function (error) {
-// //     console.log(error);
-// // });
-
 import './style.css';
 import { Client, Databases, ID } from "appwrite";
 
